dda617a78682184d919830076f6b829b50d33929
Robert-Willans <robert.willans@nice.org.uk>
2022-02-03 18:31:09 +0000

    Feat: Study def for bp variables added

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,13 @@
-from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
+from cohortextractor import (
+    StudyDefinition,
+    codelist,
+    codelist_from_csv,
+    combine_codelists,
+    filter_codes_by_category,
+    patients,
+)
 
+from codelists import *
 
 study = StudyDefinition(
     default_expectations={
@@ -7,15 +15,84 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
-    population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
-    ),
+    
+    population = patients.satisfying("""
+                                     registered AND (pat_age >= 18) AND hypertension
+                                     """,
+                                     registered = patients.registered_as_of("2021-03-31"),
+                                     pat_age = patients.age_as_of("2021-03-31"),
+                                     hypertension = patients.with_these_clinical_events(hypertension_diag_code, on_or_before = "2021-03-31")
+                                     ),
 
     age=patients.age_as_of(
-        "2019-09-01",
+        "2022-01-01",
         return_expectations={
             "rate": "universal",
             "int": {"distribution": "population_ages"},
         },
     ),
+    
+    last_systolic_bp_measure = patients.with_these_clinical_events(all_systolic_codes,
+                                                                    between = ["2020-04-01", "2021-03-31"],
+                                                                    returning = "numeric_value",
+                                                                    find_last_match_in_period = "TRUE",
+                                                                    return_expectations = {"float" : {"distribution": "normal",
+                                                                                                    "mean": 112,
+                                                                                                    "stddev": 10},
+                                                                                                    "incidence": 0.75}
+                                                                    ),
+                                                                    
+    last_systolic_bp_date = patients.with_these_clinical_events(all_systolic_codes,
+                                                                    between = ["2020-04-01", "2021-03-31"],
+                                                                    returning = "date",
+                                                                    date_format = "YYYY-MM-DD",
+                                                                    find_last_match_in_period = "TRUE",
+                                                                    return_expectations = {"date" : {"earliest": "2020-04-01",
+                                                                                                    "latest": "2021-03-31"},
+                                                                                            "rate": "exponential_increase",
+                                                                                            "incidence": 0.75}
+                                                                    ),
+                                                                
+    last_ambulatory_systolic_bp_measure = patients.with_these_clinical_events(ambulatory_systolic_codes,
+                                                                    between = ["2020-04-01", "2021-03-31"],
+                                                                    returning = "numeric_value",
+                                                                    find_last_match_in_period = "TRUE",
+                                                                    return_expectations = {"float" : {"distribution": "normal",
+                                                                                                    "mean": 112,
+                                                                                                    "stddev": 10},
+                                                                                                    "incidence": 0.75}
+                                                                    ),
+                                                                    
+    last_ambulatory_systolic_bp_date = patients.with_these_clinical_events(ambulatory_systolic_codes,
+                                                                    between = ["2020-04-01", "2021-03-31"],
+                                                                    returning = "date",
+                                                                    date_format = "YYYY-MM-DD",
+                                                                    find_last_match_in_period = "TRUE",
+                                                                    return_expectations = {"date" : {"earliest": "2020-04-01",
+                                                                                                    "latest": "2021-03-31"},
+                                                                                            "rate": "exponential_increase",
+                                                                                            "incidence": 0.75}
+                                                                    ),    
+                                                                    
+    last_ambulatory_systolic_bp_measure_narrow = patients.with_these_clinical_events(ambulatory_systolic_codes_narrow,
+                                                                    between = ["2020-04-01", "2021-03-31"],
+                                                                    returning = "numeric_value",
+                                                                    find_last_match_in_period = "TRUE",
+                                                                    return_expectations = {"float" : {"distribution": "normal",
+                                                                                                    "mean": 112,
+                                                                                                    "stddev": 10},
+                                                                                                    "incidence": 0.75}
+                                                                    ),
+                                                                    
+    last_ambulatory_systolic_bp_date_narrow = patients.with_these_clinical_events(ambulatory_systolic_codes_narrow,
+                                                                    between = ["2020-04-01", "2021-03-31"],
+                                                                    returning = "date",
+                                                                    date_format = "YYYY-MM-DD",
+                                                                    find_last_match_in_period = "TRUE",
+                                                                    return_expectations = {"date" : {"earliest": "2020-04-01",
+                                                                                                    "latest": "2021-03-31"},
+                                                                                            "rate": "exponential_increase",
+                                                                                            "incidence": 0.75}
+                                                                    ),    
+    
 )
