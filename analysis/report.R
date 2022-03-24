e8869096f48fb9eab34e453326811b21db9487d8
Robert-Willans <robert.willans@nice.org.uk>
2022-03-24 18:12:05 +0000

    fix report.R to write csv files

--- analysis/report.R
+++ analysis/report.R
@@ -1,4 +1,4 @@
-library('tidyverse')
+library(tidyverse)
 
 df_input <- read_csv(
   here::here("output", "input.csv"),
@@ -36,50 +36,50 @@ summary(data$last_ambulatory_systolic_bp_measure)
 
 data$bp140 <- ifelse(data$last_ambulatory_systolic_bp_measure<140,1,0)
 
-data$ambp140.narrow <- ifelse(data$last_ambulatory_systolic_bp_measure_narrow<140,1,0)
+data$ambp140_narrow <- ifelse(data$last_ambulatory_systolic_bp_measure_narrow<140,1,0)
 
-data$ambp140.broad <- ifelse(data$last_ambulatory_systolic_bp_measure<140,1,0)
+data$ambp140_broad <- ifelse(data$last_ambulatory_systolic_bp_measure<140,1,0)
 
 
 # calculate SBP under 135 using different AMBP/HBPM
 
-data$ambp135.narrow <- ifelse(data$last_ambulatory_systolic_bp_measure_narrow<135,1,0)
+data$ambp135_narrow <- ifelse(data$last_ambulatory_systolic_bp_measure_narrow<135,1,0)
 
-data$ambp135.broad <- ifelse(data$last_ambulatory_systolic_bp_measure<135,1,0)
+data$ambp135_broad <- ifelse(data$last_ambulatory_systolic_bp_measure<135,1,0)
 
 
-summary(as.factor(data$ambp140.narrow))
+summary(as.factor(data$ambp140_narrow))
 
-summary(as.factor(data$ambp140.broad))
+summary(as.factor(data$ambp140_broad))
 
-summary(as.factor(data$ambp135.narrow))
+summary(as.factor(data$ambp135_narrow))
 
-summary(as.factor(data$ambp135.broad))
+summary(as.factor(data$ambp135_broad))
 
 summary(as.factor(data$bp140))
 
 
 ## Create table with SBP between 140 and 135
 
-all.bp.narrow <- table(data$ambp140.narrow, data$ambp135.narrow,
+all_bp_narrow <- table(data$ambp140_narrow, data$ambp135_narrow,
                 useNA = "ifany",
                 dnn = c("under140","under135"))
 
-all.bp.narrow <- data.frame(all.bp.narrow)
+all_bp_narrow <- data.frame(all_bp_narrow)
 
 
-write.csv(all.bp.narrow, file="all.bp.narrow.csv")
+write_csv(all_bp_narrow, "output/csv_all_bp_narrow.csv")
 
 
 
-all.bp.broad <- table(data$ambp140.broad, data$ambp135.broad,
+all_bp_broad <- table(data$ambp140_broad, data$ambp135_broad,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-all.bp.broad <- data.frame(all.bp.broad)
+all_bp_broad <- data.frame(all_bp_broad)
 
 
-write.csv(all.bp.broad, file="all.bp.broad.csv")
+write_csv(all_bp_broad, "output/csv_all_bp_broad.csv")
 
 
 ## stratified analysis by CVD, CKD and DM
@@ -104,61 +104,60 @@ t2d <- table(data$bp140, data$T2D_code,useNA = "ifany", dnn = c("SBP140","T2D"))
 t2d <- data.frame(t2d)
 
 
-bp.subgroups <- data.frame(ckd, cvd, t2d, t2d)
+bp_subgroups <- data.frame(ckd, cvd, t2d, t2d)
 
-write.csv(bp.subgroups, file="bp.subgroups.csv")
+write_csv(bp_subgroups, "output/csv_bp_subgroups.csv")
 
 
 ## SBP between 140 and 135
 
 data.ckd <- data[data$CKD_code==1,]
 
-ckd.bp.narrow <- table(data.ckd$ambp140.narrow, data.ckd$ambp135.narrow,
+ckd_bp_narrow <- table(data.ckd$ambp140_narrow, data.ckd$ambp135_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-ckd.bp.narrow <- data.frame(ckd.bp.narrow)
+ckd_bp_narrow <- data.frame(ckd_bp_narrow)
 
 
-write.csv(ckd.bp.narrow, file="ckd.bp.narrow.csv")
-
+write_csv(ckd_bp_narrow, "output/csv_ckd_bp_narrow.csv")
 
 
 data.cvd <- data[data$CVD_code==1,]
 
-cvd.bp.narrow <- table(data.cvd$ambp140.narrow, data.cvd$ambp135.narrow,
+cvd_bp_narrow <- table(data.cvd$ambp140_narrow, data.cvd$ambp135_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-cvd.bp.narrow <- data.frame(cvd.bp.narrow)
+cvd_bp_narrow <- data.frame(cvd_bp_narrow)
 
 
-write.csv(cvd.bp.narrow, file="cvd.bp.narrow.csv")
+write_csv(cvd_bp_narrow, "output/csv_cvd_bp_narrow.csv")
 
 
 
 data.t1d <- data[data$T1D_code==1,]
 
-t1d.bp.narrow <- table(data.t1d$ambp140.narrow, data.t1d$ambp135.narrow,
+t1d_bp_narrow <- table(data.t1d$ambp140_narrow, data.t1d$ambp135_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-t1d.bp.narrow <- data.frame(t1d.bp.narrow)
+t1d_bp_narrow <- data.frame(t1d_bp_narrow)
 
 
-write.csv(t1d.bp.narrow, file="t1d.bp.narrow.csv")
+write_csv(t1d_bp_narrow, "output/csv_t1d_bp_narrow.csv")
 
 
 data.t2d <- data[data$T2D_code==1,]
 
-t2d.bp.narrow <- table(data.t2d$ambp140.narrow, data.t2d$ambp135.narrow,
+t2d_bp_narrow <- table(data.t2d$ambp140_narrow, data.t2d$ambp135_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-t2d.bp.narrow <- data.frame(t2d.bp.narrow)
+t2d_bp_narrow <- data.frame(t2d_bp_narrow)
 
 
-write.csv(t2d.bp.narrow, file="t2d.bp.narrow.csv")
+write_csv(t2d_bp_narrow, "output/csv_t2d_bp_narrow.csv")
 
 
 #### calculate outcomes for over 80
@@ -174,51 +173,51 @@ summary(data80$last_ambulatory_systolic_bp_measure_narrow)
 
 data80$bp150 <- ifelse(data80$last_ambulatory_systolic_bp_measure<150,1,0)
 
-data80$ambp150.narrow <- ifelse(data80$last_ambulatory_systolic_bp_measure_narrow<150,1,0)
+data80$ambp150_narrow <- ifelse(data80$last_ambulatory_systolic_bp_measure_narrow<150,1,0)
 
-data80$ambp150.broad <- ifelse(data80$last_ambulatory_systolic_bp_measure<150,1,0)
+data80$ambp150_broad <- ifelse(data80$last_ambulatory_systolic_bp_measure<150,1,0)
 
 
 
 # calculate SBP under 145 using different AMBP/HBPM
 
-data80$ambp145.narrow <- ifelse(data80$last_ambulatory_systolic_bp_measure_narrow<145,1,0)
+data80$ambp145_narrow <- ifelse(data80$last_ambulatory_systolic_bp_measure_narrow<145,1,0)
 
-data80$ambp145.broad <- ifelse(data80$last_ambulatory_systolic_bp_measure<145,1,0)
+data80$ambp145_broad <- ifelse(data80$last_ambulatory_systolic_bp_measure<145,1,0)
 
 
-summary(as.factor(data80$ambp150.narrow))
+summary(as.factor(data80$ambp150_narrow))
 
-summary(as.factor(data80$ambp150.broad))
+summary(as.factor(data80$ambp150_broad))
 
-summary(as.factor(data80$ambp145.narrow))
+summary(as.factor(data80$ambp145_narrow))
 
-summary(as.factor(data80$ambp145.broad))
+summary(as.factor(data80$ambp145_broad))
 
 summary(as.factor(data80$bp150))
 
 
 ## Create table with SBP between 150 and 145
 
-all.bp.narrow80 <- table(data80$ambp150.narrow, data80$ambp145.narrow,
+all_bp_narrow80 <- table(data80$ambp150_narrow, data80$ambp145_narrow,
                        useNA = "ifany",
                        dnn = c("under150","under145"))
 
-all.bp.narrow80 <- data.frame(all.bp.narrow80)
+all_bp_narrow80 <- data.frame(all_bp_narrow80)
 
 
-write.csv(all.bp.narrow80, file="all.bp.narrow80.csv")
+write_csv(all_bp_narrow80, "output/csv_all_bp_narrow80.csv")
 
 
 
-all.bp.broad80 <- table(data80$ambp150.broad, data80$ambp145.broad,
+all_bp_broad80 <- table(data80$ambp150_broad, data80$ambp145_broad,
                       useNA = "ifany",
                       dnn = c("under150","under145"))
 
-all.bp.broad80 <- data.frame(all.bp.broad80)
+all_bp_broad80 <- data.frame(all_bp_broad80)
 
 
-write.csv(all.bp.broad80, file="all.bp.broad80.csv")
+write_csv(all_bp_broad80, "output/csv_all_bp_broad80.csv")
 
 
 
@@ -244,58 +243,58 @@ t2d80 <- table(data80$bp150, data80$T2D_code,useNA = "ifany", dnn = c("SBP150","
 t2d80 <- data.frame(t2d80)
 
 
-bp.subgroups80 <- data.frame(ckd80, cvd80, t2d80, t2d80)
+bp_subgroups80 <- data.frame(ckd80, cvd80, t2d80, t2d80)
 
-write.csv(bp.subgroups80, file="bp.subgroups80.csv")
+write_csv(bp_subgroups80, "output/csv_bp_subgroups80.csv")
 
 
 ## SBP between 150 and 145
 
 data.ckd80 <- data80[data80$CKD_code==1,]
 
-ckd.bp.narrow80 <- table(data.ckd80$ambp150.narrow, data.ckd80$ambp145.narrow,
+ckd_bp_narrow80 <- table(data.ckd80$ambp150_narrow, data.ckd80$ambp145_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-ckd.bp.narrow80 <- data.frame(ckd.bp.narrow80)
+ckd_bp_narrow80 <- data.frame(ckd_bp_narrow80)
 
 
-write.csv(ckd.bp.narrow80, file="ckd.bp.narrow80.csv")
+write_csv(ckd_bp_narrow80, "output/csv_ckd_bp_narrow80.csv")
 
 
 
 data.cvd80 <- data80[data80$CVD_code==1,]
 
-cvd.bp.narrow80 <- table(data.cvd80$ambp150.narrow, data.cvd80$ambp145.narrow,
+cvd_bp_narrow80 <- table(data.cvd80$ambp150_narrow, data.cvd80$ambp145_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-cvd.bp.narrow80 <- data.frame(cvd.bp.narrow80)
+cvd_bp_narrow80 <- data.frame(cvd_bp_narrow80)
 
 
-write.csv(cvd.bp.narrow80, file="cvd.bp.narrow80.csv")
+write_csv(cvd_bp_narrow80, "output/csv_cvd_bp_narrow80.csv")
 
 
 
 data.t1d80 <- data80[data80$T1D_code==1,]
 
-t1d.bp.narrow80 <- table(data.t1d80$ambp150.narrow, data.t1d80$ambp145.narrow,
+t1d_bp_narrow80 <- table(data.t1d80$ambp150_narrow, data.t1d80$ambp145_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-t1d.bp.narrow80 <- data.frame(t1d.bp.narrow80)
+t1d_bp_narrow80 <- data.frame(t1d_bp_narrow80)
 
 
-write.csv(t1d.bp.narrow80, file="t1d.bp.narrow80.csv")
+write_csv(t1d_bp_narrow80, "output/csv_t1d_bp_narrow80.csv")
 
 
 data.t2d80 <- data80[data80$T2D_code==1,]
 
-t2d.bp.narrow80 <- table(data.t2d80$ambp150.narrow, data.t2d80$ambp145.narrow,
+t2d_bp_narrow80 <- table(data.t2d80$ambp150_narrow, data.t2d80$ambp145_narrow,
                        useNA = "ifany",
                        dnn = c("under140","under135"))
 
-t2d.bp.narrow80 <- data.frame(t2d.bp.narrow80)
+t2d_bp_narrow80 <- data.frame(t2d_bp_narrow80)
 
 
-write.csv(t2d.bp.narrow80, file="t2d.bp.narrow80.csv")
+write_csv(t2d_bp_narrow80, "output/csv_t2d_bp_narrow80.csv")
